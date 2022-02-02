2eac2fa1c037d4818aa18f6762bcddbea3cc8522
Mohammed Husain <mhdhusain53@gmail.com>
2022-02-02 18:03:04 +0530

    Starting Git

---
+++ src/Components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "./Navbar"
+
+const App = ()=>{
+    return (
+        <div>
+            <Navbar />
+
+            {/* <h1>
+                Black Spoon
+                <img  alt="photo"
+                    src="https://i.pinimg.com/originals/da/00/49/da0049fd6948a0a7b84060fc65cd5ec3.jpg" />
+            </h1> */}
+        </div>
+        
+    )
+}
+
+export default App;
---
+++ src/Components/Card.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+const Card = ()=>{
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Card;
